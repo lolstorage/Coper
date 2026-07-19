2c86cc02be9ee542da37dc704ca86d3b51e2f22e
lolstorage <lolstorage16@gmail.com>
2026-07-19 22:56:03 +0600

    Update ModuleManager.java

--- src/main/java/me/cope/noobs/module/ModuleManager.java
+++ src/main/java/me/cope/noobs/module/ModuleManager.java
@@ -1,5 +1,7 @@
 package me.cope.noobs.module;
 
+import me.cope.noobs.module.modules.movement.Flight;
+import me.cope.noobs.module.modules.exploit.SP_God;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +10,9 @@ public class ModuleManager {
     private final List<Module> modules = new ArrayList<>();
 
     public void init() {
-        // Register modules here: modules.add(new FlightModule());
+        // Registering your modules
+        modules.add(new Flight());
+        modules.add(new SP_God());
     }
 
     public List<Module> getModules() {
